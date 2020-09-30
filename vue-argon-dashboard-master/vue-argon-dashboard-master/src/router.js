2f563cea416d61a13eaa86c6737efa4f318077bf
Pisethpanha Chhean <chheanpisethpanha17@kit.edu.kh>
2020-09-30 15:27:34 +0700

    front-end template

---
+++ vue-argon-dashboard-master/vue-argon-dashboard-master/src/router.js
@@ -0,0 +1,63 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import DashboardLayout from '@/layout/DashboardLayout'
+import AuthLayout from '@/layout/AuthLayout'
+Vue.use(Router)
+
+export default new Router({
+  linkExactActiveClass: 'active',
+  routes: [
+    {
+      path: '/',
+      redirect: 'overview',
+      component: DashboardLayout,
+      children: [
+        {
+          path: '/overview',
+          name: 'overview',
+          // route level code-splitting
+          // this generates a separate chunk (about.[hash].js) for this route
+          // which is lazy-loaded when the route is visited.
+          component: () => import(/* webpackChunkName: "demo" */ './views/Dashboard.vue')
+        },
+        {
+          path: '/calendar',
+          name: 'calendar',
+          component: () => import(/* webpackChunkName: "demo" */ './views/Icons.vue')
+        },
+        {
+          path: '/patient-list',
+          name: 'patient-list',
+          component: () => import(/* webpackChunkName: "demo" */ './views/UserProfile.vue')
+        },
+        {
+          path: '/payment-info',
+          name: 'payment-info',
+          component: () => import(/* webpackChunkName: "demo" */ './views/Maps.vue')
+        },
+        {
+          path: '/settings',
+          name: 'settings',
+          component: () => import(/* webpackChunkName: "demo" */ './views/Tables.vue')
+        }
+      ]
+    },
+    {
+      path: '/',
+      redirect: 'login',
+      component: AuthLayout,
+      children: [
+        {
+          path: '/login',
+          name: 'login',
+          component: () => import(/* webpackChunkName: "demo" */ './views/Login.vue')
+        },
+        {
+          path: '/register',
+          name: 'register',
+          component: () => import(/* webpackChunkName: "demo" */ './views/Register.vue')
+        }
+      ]
+    }
+  ]
+})
