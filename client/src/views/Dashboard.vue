dba3c2883488be8df15b0b4b037972eef80a12b9
sokmengpsm <34527171+sokmengpsm@users.noreply.github.com>
2020-10-01 22:13:12 +0700

    Dashboard linechart and card

--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -7,8 +7,8 @@
                 <card type="default" header-classes="bg-transparent">
                         <div slot="header" class="row align-items-center">
                             <div class="col">
-                                <h6 class="text-light text-uppercase ls-1 mb-1">Overview</h6>
-                                <h5 class="h3 text-white mb-0">Sales value</h5>
+                                <h6 class="text-light text-uppercase ls-1 mb-1">Welcome Back, Dr. </h6>
+                                <h5 class="h3 text-white mb-0">APPOINTMENTS STATISTICS</h5>
                             </div>
                             <div class="col">
                                 <ul class="nav nav-pills justify-content-end">
@@ -50,111 +50,82 @@
         <div class="container-fluid mt--7">
             <div class="row">
                   <div class="col-xl-3">
-                    <stats-card title="Total traffic"
-                                type="gradient-red"
-                                sub-title="350,897"
-                                icon="ni ni-active-40"
-                                class="mb-4 mb-xl-0"
-                    >
-
-                        <template slot="footer">
-                            <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
-                            <span class="text-nowrap">Since last month</span>
-                        </template>
-                    </stats-card>
-                </div>
-                <div class="col-xl-3">
-                    <stats-card title="Total traffic"
-                                type="gradient-orange"
-                                sub-title="2,356"
-                                icon="ni ni-chart-pie-35"
-                                class="mb-4 mb-xl-0"
-                    >
-
-                        <template slot="footer">
-                            <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 12.18%</span>
-                            <span class="text-nowrap">Since last month</span>
-                        </template>
-                    </stats-card>
-                </div>
-                <div class="col-xl-3">
-                    <stats-card title="Sales"
-                                type="gradient-green"
-                                sub-title="924"
-                                icon="ni ni-money-coins"
-                                class="mb-4 mb-xl-0"
-                    >
-
-                        <template slot="footer">
-                            <span class="text-danger mr-2"><i class="fa fa-arrow-down"></i> 5.72%</span>
-                            <span class="text-nowrap">Since last month</span>
-                        </template>
-                    </stats-card>
-
-                </div>
-
-                <div class="col-xl-3">
-                    <stats-card title="Sales"
-                                type="gradient-green"
-                                sub-title="924"
-                                icon="ni ni-money-coins"
-                                class="mb-4 mb-xl-0"
-                    >
-
-                        <template slot="footer">
-                            <span class="text-danger mr-2"><i class="fa fa-arrow-down"></i> 5.72%</span>
-                            <span class="text-nowrap">Since last month</span>
-                        </template>
-                    </stats-card>
-
-                </div>
-                <!-- <div class="col-xl-3 col-lg-6">
-                    <stats-card title="Performance"
-                                type="gradient-info"
-                                sub-title="49,65%"
-                                icon="ni ni-chart-bar-32"
-                                class="mb-4 mb-xl-0"
-                    >
-
-                        <template slot="footer">
-                            <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 54.8%</span>
-                            <span class="text-nowrap">Since last month</span>
-                        </template>
-                    </stats-card>
-                </div> -->
-
-                    
-
-                <!-- <div class="col-xl-3">
-                    <card header-classes="bg-transparent">
-                        <div slot="header" class="row align-items-center">
-                            <div class="col">
-                                <h6 class="text-uppercase text-muted ls-1 mb-1">Performance</h6>
-                                <h5 class="h3 mb-0">Total orders</h5>
-                            </div>
+                    <div class="card mb-4 mb-xl-0">
+                      <div class="card-body">
+                        <h3 class="card-title">Card title</h3>
+                        <div class="row custom-center">
+                          <div class="col" >
+                              <p style="text-align: center;font-size:60px;">5</p>
+                          </div>
+                          <div class="col ">
+                              <div class="row pt-3 mb-2" style="background:#F2F5FA">
+                                <div class="col">
+                                  <p style=";font-size:10px">Consultation</p>
+                                </div>
+                                <div class="col">
+                                  <p style=";font-size:10px">9:00 - 10:00</p>
+                                </div>
+                              </div>
+                              <div class="row pt-3 mb-2" style="background:#F2F5FA">
+                                <div class="col">
+                                  <p style=";font-size:10px">Open Access</p>
+                                </div>
+                                <div class="col">
+                                  <p style=";font-size:10px">11:00 - 12:00</p>
+                                </div>
+                              </div>
+                              <div class="row pt-3 mb-2" style="background:#F2F5FA">
+                                <div class="col">
+                                  <p style=";font-size:10px">Scaling</p>
+                                </div>
+                                <div class="col">
+                                  <p style=";font-size:10px">13:00 - 15:00</p>
+                                </div>
+                              </div>
+                          </div>
                         </div>
+                      </div>
+                    </div>
+                </div>
+                <div class="col-xl-3">
+                    <div class="card mb-4 mb-xl-0">
+                      <div class="card-body mb-1">
+                        <h3 class="card-title">TOP TREATMENT</h3>
+                        <div style="font-size:10px">
+                          <p>1. Consultation</p>
+                          <p>2. Scaling</p>
+                          <p>3. Root Canal</p>
+                          <p>4. Bleaching</p>
+                          <p>5. Wisdom Teeth Removal</p>
+                        </div>
+                      </div>
+                    </div>
+                </div>
+                <div class="col-xl-3">
+                    <div class="card mb-4 mb-xl-0">
+                      <div class="card-body mb-2">
+                        <h3 class="card-title">TOP PATIENTS THIS MON</h3>
+                        <p style="font-size:40px">10</p>
+                        <h3 class="card-title">TOP PATIENTS ALL TIME</h3>
+                        <p style="font-size:40px">10</p>
+                      </div>
+                    </div>
+                </div>
 
-                        <bar-chart
-                                :height="350"
-                                ref="barChart"
-                                :chart-data="redBarChart.chartData"
-                        >
-                        </bar-chart>
-                    </card>
-                </div> -->
+                <div class="col-xl-3">
+                    <div class="card mb-4 mb-xl-0">
+                      <div class="card-body mb-2">
+                        <h3 class="card-title">TOTAL INCOME</h3>
+                        <p style="font-size:30px">$23,000</p>
+                        <h5 class="card-title">This Month So Far</h5>
+                        <p style="font-size:30px">$35,000</p>
+                        <h5 class="card-title">Previous Month</h5>
+                      </div>
+                    </div>
+
+                </div>
             </div>
             <!-- End charts-->
-
-            <!--Tables-->
-            <!-- <div class="row mt-5">
-                <div class="col-xl-8 mb-5 mb-xl-0">
-                    <page-visits-table></page-visits-table>
-                </div>
-                <div class="col-xl-4">
-                    <social-traffic-table></social-traffic-table>
-                </div>
-            </div> -->
-            <!--End tables-->
         </div>
 
     </div>
@@ -163,42 +134,30 @@
   // Charts
   import * as chartConfigs from '@/components/Charts/config';
   import LineChart from '@/components/Charts/LineChart';
-  // import BarChart from '@/components/Charts/BarChart';
-
-  // Tables
-  // import SocialTrafficTable from './Dashboard/SocialTrafficTable';
-  // import PageVisitsTable from './Dashboard/PageVisitsTable';
 
   export default {
     components: {
       LineChart,
-      // BarChart,
-      // PageVisitsTable,
-      // SocialTrafficTable,
+    
     },
     data() {
       return {
         bigLineChart: {
-          allData: [
+          allData1: [
             [0, 20, 10, 30, 15, 40, 20, 60, 60],
             [0, 20, 5, 25, 10, 30, 15, 40, 40]
+          ],
+          allData2: [
+            [10, 20, 10, 30, 15, 40, 20, 10, 80],
+            [0, 20, 5, 25, 10, 30, 15, 40, 10]
           ],
           activeIndex: 0,
           chartData: {
             datasets: [],
             labels: [],
           },
-          extraOptions: chartConfigs.blueChartOptions,
+          extraOptions: chartConfigs.lineChartOptionsBlue,
         },
-        redBarChart: {
-          chartData: {
-            labels: ['Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-            datasets: [{
-              label: 'Sales',
-              data: [25, 20, 30, 22, 17, 29]
-            }]
-          }
-        }
       };
     },
     methods: {
@@ -206,11 +165,19 @@
         let chartData = {
           datasets: [
             {
-              label: 'Performance',
-              data: this.bigLineChart.allData[index]
+              label: 'Appointments',
+              data: this.bigLineChart.allData1[index],
+              backgroundColor:'rgba(54, 162, 235, 0.2)',
+              borderColor:'rgba(54, 162, 235, 1)'
+            },
+            {
+              label: 'Cancel',
+              data: this.bigLineChart.allData2[index],
+              backgroundColor:'rgba(255, 99, 132, 0.2)',
+              borderColor:'rgba(255,99,132,1)'
             }
           ],
-          labels: ['May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+          labels: ['Jan','Feb','Mar','Apr','May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
         };
         this.bigLineChart.chartData = chartData;
         this.bigLineChart.activeIndex = index;
@@ -221,4 +188,9 @@
     }
   };
 </script>
-<style></style>
+<style>
+.custom-center{
+    display: flex;
+    align-items: center;
+}
+</style>
